4589f4c65fd2fada120cbb899a4d636d2fd8e3c7
KarthikRaju14 <rajuraju1412@gmail.com>
2023-03-23 21:59:42 +0530

    auto

---
+++ Test_Data/data.py
@@ -0,0 +1,147 @@
+class Orange_Data:
+    #Test_case_1
+    #Orange HRM login data
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    username = 'Admin'
+    password = 'admin123'
+    #Admin lower & upper case
+    admin_upper ='ADMIN'
+    admin_lower ='admin'
+    #PIM lower & upper case
+    pim_upper = 'PIM'
+    pim_lower = 'pim'
+    #Leave lower & upper case
+    leave_upper = 'LEAVE'
+    leave_lower = 'leave'
+    #Time lower & upper case
+    time_upper = 'TIME'
+    time_lower = 'time'
+    #Recruitment lower & upper case
+    recruitment_upper = 'RECRUITMENT'
+    recruitment_lower = 'recruitment'
+    #My Info lower & upper case
+    myinfo_upper = 'MY INFO'
+    myinfo_lower = 'my info'
+    #Performance lower & upper case
+    performance_upper = 'PERFORMANCE'
+    performance_lower = 'performance'
+    #Dashboard lower & upper case
+    dashboard_upper = 'DASHBOARD'
+    dashboard_lower = 'dashboard'
+    #Directory lower & upper case
+    directory_upper = 'DIRECTORY'
+    directory_lower = 'directory'
+    #Maintenance lower & upper case
+    maintenance_upper = 'MAINTENANCE'
+    maintenance_password = 'admin123'
+    maintenance_lower = 'maintenance'
+    #Buzz lower & upper case
+    buzz_upper = 'BUZZ'
+    buzz_lower = 'buzz'
+
+    #Test_case_2
+    #we don't need data rather then admin for enabling and disabling functionality
+    u_name = 'Admin'
+
+    #Test_case_3
+    #first name data
+    first_name = 'Travis'
+    #last name data
+    last_name = 'Scott'
+    #create login details for username data and password data 
+    login_username = 'Travisscott'
+    #password data
+    login_password = 'Travis123$'
+    #confirmation password data
+    confirm_password = 'Travis123$'
+
+    #Test_case_4
+    #employee name data
+    woker_name = 'Travis'
+
+    #Test_case_5
+    #other id data
+    other_id = '234567'
+    #driver's license number data
+    dlicense_number = '765432'
+    #licence expiry date data
+    license_edate = '2023-03-20'
+    #ssn number data
+    ssn_number = '2345678'
+    #sin number data
+    sin_number ='8765432'
+    #date of birth data
+    dob = '1995-05-05'
+    #military service data
+    military_service = 'no'
+
+    #Test_case_6
+    #street data
+    street = '932 Gerrard St E'
+    #street name data
+    street_name = 'north west street'
+    #city name data
+    city_name = 'Toronto'
+    #state name data
+    state_name = 'Ontario'
+    #Zip code data
+    zip_code = 'M4M 1Z2'
+    #home number data
+    home_number = '453689'
+    #mobile number data
+    mobile_number = '4164661209'
+    #work name data
+    work_name = 'scott'
+    #work mail id data
+    work_mailid = 'travisscott05@gmail.com'
+    #other mail id data
+    other_mail = 'scottjones@outlook.com'
+
+    #Test_Case_7
+    #name of emergency contact data
+    name = 'Kylie Jenner'
+    #Realationship data
+    relationship = 'wife'
+    #home telephone data
+    home_telephone = '9687-543678'
+    #mobile number data
+    mobile_number1 = '7756982134'
+    #work telephone data
+    work_telephone = '6582-968745'
+
+    #Test_Case_8
+    #dependents name data
+    dependent_name = 'Stormi Webster'
+    #relationship Date of birth
+    realtion_dob = '2020-05-26'
+    
+    #Test_Case_9
+    #job joined date data
+    joined_date = '2019-03-19'
+
+    #Test_Case_10
+    #Termination date data
+    termination_d = '2023-01-25'
+    #Termination note data
+    terminate_note = 'I have got an job in better package. Thank you friends'
+
+    #Test_Case_11
+    #test case is only activating case so don't of data all data are in before test case
+
+    #Test_Case_12
+    #salary component data
+    salary_component = '3598'
+    #salart amount data
+    salary_amount = '3598'
+    #account number data
+    account_number = '66981436275'
+    #routing number data
+    routing_number = '982534196'
+    #deposit amount data
+    r_amount = '3598'
+
+    #Test_Case_13
+    #federal income tax exemption data
+    fit_exemption = '12'
+    #sate income tax exemption data
+    sit_exemption = '6'
